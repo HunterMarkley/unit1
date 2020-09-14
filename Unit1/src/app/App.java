cd1adb0b0e8a2577845fc7a08ee1b11771378e7d
HunterMarkley <825068@mystma.org>
2020-09-14 16:51:04 +0000

    added 5 favorite movies

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1. Fast and furious toyko drift");
+        System.out.println("2. Baby Driver");
+        System.out.println("3. Bad Boys for Life");
+        System.out.println("4. John Wick Trilogy");
+        System.out.println("5. Hobbs and Shaw");
     }
 }
