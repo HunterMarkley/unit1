12c35e85bfe3a75c6a2cdc6adb7417c570335174
HunterMarkley <825068@mystma.org>
2020-09-24 17:54:43 +0000

    added variables and data types

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,8 +4,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         Boolean livesInAlbertville = false;
         int myFavNum = 240;
-    
-    
+        char myFirstLetterOfMyCar = 's';
+        byte howManySeatsInMyCar = 5;
+        short howTallImI = 70;
+        long theLongestIveDriven = 2000;
+        float howManySongsDoIPlayPerDay = (float) 200.3576;
+        double howManyPiecesMyCarIsGonnaBeInOverWinter = 100.7374325432;
+
+    System.out.println(howManyPiecesMyCarIsGonnaBeInOverWinter);    
+    System.out.println(howManySongsDoIPlayPerDay);    
+    System.out.println(theLongestIveDriven);    
+    System.out.println(howTallImI);     
+    System.out.println(howManySeatsInMyCar);
+    System.out.println(myFirstLetterOfMyCar);
     System.out.println(myFavNum);
     System.out.println(livesInAlbertville);}
 }
