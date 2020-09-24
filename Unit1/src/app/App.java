13ad925b43d9ec3d4f17437eaf8886fdf4330e1b
HunterMarkley <825068@mystma.org>
2020-09-24 18:27:22 +0000

    starting mad lib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,21 +2,8 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Boolean livesInAlbertville = false;
-        int myFavNum = 240;
-        char myFirstLetterOfMyCar = 's';
-        byte howManySeatsInMyCar = 5;
-        short howTallImI = 70;
-        long theLongestIveDriven = 2000;
-        float howManySongsDoIPlayPerDay = (float) 200.3576;
-        double howManyPiecesMyCarIsGonnaBeInOverWinter = 100.7374325432;
+    String firstName = "Hunter";
+    String lastName = "Markley";
 
-    System.out.println(howManyPiecesMyCarIsGonnaBeInOverWinter);    
-    System.out.println(howManySongsDoIPlayPerDay);    
-    System.out.println(theLongestIveDriven);    
-    System.out.println(howTallImI);     
-    System.out.println(howManySeatsInMyCar);
-    System.out.println(myFirstLetterOfMyCar);
-    System.out.println(myFavNum);
-    System.out.println(livesInAlbertville);}
-}
+    System.out.println(firstName + " " + lastName);
+    }
