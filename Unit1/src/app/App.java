b8bc3ff06001051f677b6c925fd1bd43c5384fc5
HunterMarkley <825068@mystma.org>
2020-10-29 21:46:04 +0000

    While loops continued

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,16 +3,14 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
     
-    int count = 5;
+    
     String color = "black";
 
-    while (count < 10){
-        System.out.println("The count is " + count);
-        count = count + 1;
-    }
 
     while (color == "black"){
-        System.out.println( "color is " + color);
+        System.out.println( "black " + color);
+        System.out.println( "black " + color);
+        System.out.println( "black " + color);
         color = "blue";
     }
 
