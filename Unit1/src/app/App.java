92c0726424e6d47cd6a8414e7ec8496264606c3a
HunterMarkley <825068@mystma.org>
2020-10-29 22:43:32 +0000

    finished for loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,7 +4,27 @@ public class App {
     public static void main(String[] args) throws Exception {
     
 
-        for (int i = 10; i <= 20; i = 10 +1){
+        for (int i = 10; i <= 20; i = i +1){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
+        }
+
+        for (int i = 10; i <= 100; i = i +1){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
+        }
+
+        for (int i = 1000; i <= 256000; i = i +1){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
+        }
+
+        for (int i = 11; i <= 12; i = i +1){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
+        }
+
+        for (int i = 10; i <= 0; i = i -1){
             System.out.println("The count is " + i);
             System.out.println("Let's keep counting.");
         }
