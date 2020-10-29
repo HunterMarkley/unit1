880ef991645c1ef8247752a82377e666682e36ce
HunterMarkley <825068@mystma.org>
2020-10-29 20:09:25 +0000

    working on second while loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,32 +3,18 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
     
-        int count = 0;
-        String color = "black";
-        String vehicle = "240sx";
+    int count = 5;
+    String color = "black";
 
-        while (count < 10) {
-            System.out.println("The count is " + count);
-            count = count + 1;
-        }
+    while (count < 10){
+        System.out.println("The count is " + count);
+        count = count + 1;
+    }
 
-
-        System.out.println("count finished");
-
-
-        while (color == "black"){
-            System.out.println("The color is " + color);
-            color = "blue";
-        }
-
-        System.out.println("color finished");
-
-        while (vehicle == "240sx"){
-            System.out.println("The vehicle is " + vehicle);
-            vehicle = "180sx";
-        }
-
-        System.out.println("vehicle is finished");
+    while (color == "black"){
+        System.out.println( "color is " + color);
+        color = "blue";
+    }
 
     }
 }
