534691223a7580c9090166e436db899f71c3dcba
HunterMarkley <825068@mystma.org>
2020-10-29 19:04:07 +0000

    while loops first assignment

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,8 +2,33 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-    String firstName = "Hunter";
-    String lastName = "Markley";
+    
+        int count = 0;
+        String color = "black";
+        Tuples vehicle = "240sx";
 
-    System.out.println(firstName + " " + lastName);
+        while (count < 10) {
+            System.out.println("The count is " + count);
+            count = count + 1;
+        }
+
+
+        System.out.println("count finished");
+
+
+        while (color == "black"){
+            System.out.println("The color is " + color);
+            color = "blue";
+        }
+
+        System.out.println("color finished");
+
+        while (vehicle = "240sx"){
+            System.out.println("The vehicle is " + vehicle);
+            vehicle = "180sx";
+        }
+
+        System.out.println("vehicle is finished");
+
     }
+}
