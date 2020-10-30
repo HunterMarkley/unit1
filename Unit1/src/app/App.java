c029faa07e27a533bbef6f9b5c3c619576f9b64f
HunterMarkley <825068@mystma.org>
2020-10-30 18:21:34 +0000

    animal facts

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -8,16 +8,13 @@ public class App {
 
 
         System.out.println("Welcome to Dog facts please choose a fact (1-5)");
-        String name = input.nextLine();
-        System.out.println("If u picked number 1 this is your fact");
-        System.out.println("Dogs can read our emotions");
-        System.out.println("If u picked number 2 this is your fact");
-        System.out.println("A dogs sense of smell is 10,000 to 100,000 stronger then humans");
-        System.out.println("If u picked number 3 this is your fact");
-        System.out.println("Dogs are as smart as a 2 year old child");
-        System.out.println("If u picked number 4 this is your fact");
-        System.out.println("Dogs sweat through their paws");
-        System.out.println("If u picked number 5 this is your fact");
-        System.out.println("Dogs can see some colors");
+         int myInt = 0;
+         String name = input.nextLine();
+       
+        if (myInt <= 1){
+            System.out.println("Dogs can read out emotions");
+
+        }
+        
     }
 }
